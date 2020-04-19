4999c30e7fc336b9372cea7b986fb0287da82743
salekin01 <salekin01@outlook.com>
2020-04-20 00:18:43 +0200

    java-biginteger

---
+++ Java Practice/java-biginteger.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+        Scanner sc = new Scanner(System.in);
+        BigInteger a = sc.nextBigInteger();
+        BigInteger b = sc.nextBigInteger();
+        sc.close();
+        System.out.println(a.add(b));
+        System.out.println(a.multiply(b));
+    }
+}
